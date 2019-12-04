8ee733a5d6b7262d5a03d1006a0b5ef3b8a10621
Grassjpg <zhanse0746@greenvilleschools.us>
2019-12-04 16:52:50 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,94 @@
+answer = input('  Do you like France\n')
+if answer == 'yes':
+   print('You are wrong.')
+elif answer == 'no':
+   print('Good') 
+   exit()
+elif answer == 'weert':
+   print('Good job, you have discovered the weert easter egg.')
+   exit()
+elif answer == '69':
+  print('nice')
+  exit()
+else:
+  print ('It is a yes or no question')
+answer = input('  Do you like France\n')
+if answer == 'yes':
+   print('You are wrong.')
+elif answer == 'no':
+   print('Good')
+   exit()
+else:
+   print('It is a yes or no question.')
+answer = input('  Do you like France\n')
+if answer == 'yes':
+   print('When will you learn...')
+elif answer == 'no':
+   print('Good')
+   exit()
+else:
+   print('It is a yes or no question.')
+answer = input('  Do you like France\n')
+if answer == 'yes':
+   print('...')
+elif answer == 'no':
+   print('Good')
+   exit()
+else:
+   print('It is a yes or no question.')
+answer = input('  Do you like France\n')
+if answer == 'yes':
+   print('')
+elif answer == 'no':
+   print('Good')
+   exit()
+else:
+   print('It is a yes or no question.')
+answer = input('  Do you like France\n')
+if answer == 'yes':
+   print('')
+elif answer == 'no':
+   print('Good')
+   exit()
+else:
+   print('It is a yes or no question.')
+answer = input('  Do you like France\n')
+if answer == 'yes':
+   yn = input(  'STOP\n')
+   if yn == 'yes':
+     print ('thank you')
+   elif yn == 'no':
+     print ('>:(')
+     exit()
+   elif yn == 'ok':
+     print ('thank you')
+   else:
+     yn = input(  'STOP\n')
+     if yn == 'yes':
+       print ('thank you')
+     elif yn == 'shut up':
+       print ('First of all, that was rude. But good job finding the shut up easter egg.')
+     elif yn == 'no':
+       print ('>:(')
+       exit()
+     elif yn == 'ok':
+      print ('thank you')
+     elif yn == 'shut up':
+       print ('First of all, that was rude. But good job finding the shut up easter egg.')
+       exit()
+     else:
+       exit()
+elif answer == 'no':
+   print('Good')
+   exit()
+else:
+   print('It is a yes or no question.')
+answer = input('  Do you like France\n')
+if answer == 'yes':
+   print('YOU SAID YOU WOULD STOP')
+   exit()
+elif answer == 'no':
+   print('Finally')
+   exit()
+else:
+   exit()
